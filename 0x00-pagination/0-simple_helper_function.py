2276b98bbb4edaff0db6ec58edaad009fa473146
NamasakaLennox <namasakalennox254@gmail.com>
2023-12-29 12:18:00 +0300

    a helper function to return start and end indexes of a file

---
+++ 0x00-pagination/0-simple_helper_function.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+"""
+The function should return a tuple of size two containing a start index and an
+end index corresponding to the range of indexes to return in a list for those
+particular pagination parameters.
+"""
+from typing import Tuple
+
+
+def index_range(page: int, page_size: int) -> Tuple[int, int]:
+    """
+    takes a page number and size and returns start and end index for that page
+    """
+    end = page * page_size
+    start = end - page_size
+
+    return (start, end)
