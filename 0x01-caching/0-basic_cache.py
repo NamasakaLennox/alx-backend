a5c2a4983127e26839aac28f06ce0a88f488a176
NamasakaLennox <namasakalennox254@gmail.com>
2024-01-02 19:15:50 +0300

    [new]: implemented a simple cache class that stores unlimited cache items

---
+++ 0x01-caching/0-basic_cache.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+"""
+Implements a basic dictionary as a cache
+"""
+BaseCaching = __import__('base_caching').BaseCaching
+
+
+class BasicCache(BaseCaching):
+    """
+    implements a dictionary for caching without a limit
+    """
+    def __init__(self):
+        """
+        constructor method for the class
+        """
+        super().__init__()
+
+    def put(self, key, item):
+        """
+        assign a value to the dictionary
+        """
+        if key:
+            self.cache_data[key] = item
+
+    def get(self, key):
+        """
+        gets a value from the dictionary
+        """
+        return self.cache_data.get(key)
